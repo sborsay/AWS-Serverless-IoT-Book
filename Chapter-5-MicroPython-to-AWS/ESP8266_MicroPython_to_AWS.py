bc14d932f31980cf856224110d06f16b0ea9f4ad
Stephen Borsay <borsay@gmail.com>
2022-07-24 16:43:56 -0700

    Create ESP8266_MicroPython_to_AWS.py

---
+++ Chapter-5-MicroPython-to-AWS/ESP8266_MicroPython_to_AWS.py
@@ -0,0 +1,107 @@
+#AWS MQTT client cert example for esp8266, this sketch is a combination of various sources:
+#https://awsiot.wordpress.com/2019/01/10/connect-8266-to-aws-mqtt-using-miropython/
+#https://forum.micropython.org/viewtopic.php?t=5166
+#Originalcode added by Stephen Borsay for Udemy Course and AWS IoT Book
+#AWS MQTT Connect Pub/Sub
+
+from umqtt.robust import MQTTClient
+import time
+import random
+import machine
+pin = machine.Pin(2)
+
+#Convert your AWS Certs with OpenSSL:  directions in the header of this code
+#Open.SSL can be found with default install  in--> C:\Program Files\Git\usr\bin>
+CERT_FILE = "/cert33.cert.der"
+KEY_FILE = "/private33.key.der"
+
+#Client_ID should be unique per device
+MQTT_CLIENT_ID = "HeavyPetter33"
+MQTT_PORT = 8883 #MQTT Secured
+
+#Pub and Sub topics 
+PUB_TOPIC = "iot/outTopic" #coming out of device
+SUB_TOPIC = "iot/inTopic"  #coming into device
+
+#Change the following three settings to match your environment
+#IoT Core-->Settings or > aws iot describe-endpoint --endpoint-type iot:Data-ATS
+MQTT_HOST = "a32qaa131oyees-ats.iot.us-west-2.amazonaws.com"  #Your AWS IoT Endpoint
+WIFI_SSID = "iPhone"
+WIFI_PW = "burger888"
+
+MQTT_CLIENT = None  #empty object
+
+print("Starting program...")
+
+def network_connect():
+    print("In network connect method")
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(WIFI_SSID , WIFI_PW)
+        while not sta_if.isconnected():
+            pass  # I do nothing and like it
+    print('network config:', sta_if.ifconfig())
+    
+
+def pub_msg(msg):  #publish is synchronous so we poll and publish
+    global MQTT_CLIENT
+    try:    
+        MQTT_CLIENT.publish(PUB_TOPIC, msg)
+        print("Sent: " + msg)
+    except Exception as e:
+        print("Exception publish: " + str(e))
+        raise
+
+def sub_cb(topic, msg):
+    print('Device received a Message: ')
+    print((topic, msg))  #print incoming message asynchronously
+    pin.value(0) #blink if incoming message by toggle off
+
+def cloud_connect():    
+    global MQTT_CLIENT
+
+    try:  #all this below runs once ,equivalent to Arduino's "setup" function)
+        with open(KEY_FILE, "r") as f: 
+            key = f.read()
+
+        print("set private key")
+            
+        with open(CERT_FILE, "r") as f: 
+            cert = f.read()
+
+        print("set client cert")
+
+        MQTT_CLIENT = MQTTClient(client_id=MQTT_CLIENT_ID, server=MQTT_HOST, port=MQTT_PORT, keepalive=5000, ssl=True, ssl_params={"cert":cert, "key":key, "server_side":False})
+        MQTT_CLIENT.connect()
+        print('Network Connected')
+        MQTT_CLIENT.set_callback(sub_cb)
+        MQTT_CLIENT.subscribe(SUB_TOPIC)
+        print('Subscribed to %s as the incoming topic' % (SUB_TOPIC))
+        return MQTT_CLIENT
+    except Exception as e:
+        print('Cannot connect MQTT: ' + str(e))
+        raise
+
+#Start execution
+try:
+    print("Connecting to WiFi Network")
+    network_connect()
+    print("Connecting to AWS IoT")
+    cloud_connect()
+    print("Entering Loop")
+    while True: #loop forever
+            pin.value(1)
+            pending_message = MQTT_CLIENT.check_msg()  #check for new subscription payload
+            if pending_message != 'None':  #check if we have a message 
+                temp = (time.time())%99   #because randomint doesn't work
+                humid = (time.time())%98  
+                deviceTime = time.time()
+                pub_msg("{\n  \"temperature\": %d,\n  \"humidity\": %d,\n  \"timestamps\": %d\n}"%(temp,humid,deviceTime))
+                print("OK Published payload")
+                time.sleep(5)  # 5 second delay between publishing
+            
+except Exception as e:
+    print(str(e))
