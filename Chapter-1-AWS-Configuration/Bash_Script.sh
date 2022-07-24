eb3e15825f23e02b321e4670fecf96ae266fffe3
Stephen Borsay <borsay@gmail.com>
2022-07-24 11:15:09 -0700

    Create Bash_Script.sh

---
+++ Chapter-1-AWS-Configuration/Bash_Script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#  Adjustedted for AWS CLI Version 2, Stephen Borsay
+#  Original code found at:
+#  https://aws.amazon.com/blogs/iot/integrating-iot-data-with-your-data-lake-with-new-aws-iot-analytics-features
+
+mqtttopic='<Insert-Your-IoT-Topic-Here>'
+iterations=10
+wait=2
+region='<Insert-Your-AWS-Test-Region-Here>'
+profile='default'
+
+for (( i = 1; i <=$iterations; i++)) {
+
+    #Added these randomizers, Temperature in Fehr
+    minT=-20
+    maxT=120
+    numberT=$(expr $minT + $RANDOM % $maxT)
+
+    #humidity % cannot exceed 100
+    minH=0
+    maxH=100
+    numberH=$(expr $minH + $RANDOM % $maxH)
+
+  temperature=$(($numberT ))
+  humidity=$(($numberH ))
+
+  echo "Publishing message $i/$ITERATIONS to IoT topic $mqtttopic:"
+  echo "temperature: $temperature"
+  echo "humidity: $humidity"
+  
+ #use below for AWS CLI V2
+ aws iot-data publish --topic "$mqtttopic" --cli-binary-format raw-in-base64-out --payload "{\"temperature\":$temperature,\"humidity\":$humidity}" --profile "$profile" --region "$region"
+
+  sleep $wait
+}
