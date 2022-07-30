869c7573ca7ddd13f6fed2bf4d537a0124ca5549
Stephen Borsay <borsay@gmail.com>
2022-07-30 12:47:24 -0700

    Update Lambda_Publisher.py

--- Chapter.10-AWS-IoT-Core-With-Lambda/Lambda_Publisher.py
+++ Chapter.10-AWS-IoT-Core-With-Lambda/Lambda_Publisher.py
@@ -1,1 +1,20 @@
-sxfb
+import json
+import boto3
+
+client = boto3.client('iot-data', region_name='<Your-AWS-Region-Here>')
+
+def lambda_handler(event, context):
+    #event['temperature'] = round(((event['temperature'] - 32) / 1.8))
+    print(event)
+    response = client.publish(
+        topic='fromLambda',
+        qos=0,
+        payload=json.dumps(event) 
+    )
+    print(response)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Published to topic')
+    }
+
