31f79a7e57570e2e104e7da9cbe96b2515459420
Stephen Borsay <borsay@gmail.com>
2022-07-31 15:21:18 -0700

    Create DynaoDB_Extractor.py

---
+++ Chapter.14-DynamoDB-For-IoT/DynaoDB_Extractor.py
@@ -0,0 +1,12 @@
+def lambda_handler(event,context):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('<YOUR-DDB-TABLE-HERE>')
+
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+    return(data)
