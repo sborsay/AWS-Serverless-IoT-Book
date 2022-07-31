3a9821c70caed8830af21d603180ec4f2f30007f
Stephen Borsay <borsay@gmail.com>
2022-07-31 09:32:04 -0700

    Create Lambda_Publisher.js

---
+++ Chapter.10-Prerequisite2-IoT-Core-With-Lambda/Lambda_Publisher.js
@@ -0,0 +1,21 @@
+var AWS = require('aws-sdk');
+var iotdata = new AWS.IotData({endpoint: '<YOUR-AWS-IOT-ENDPOINT-HERE>' });
+
+exports.handler = (event) => {
+    //event['temperature'] = round(((event['temperature'] - 32) / 1.8))
+    console.log("The event object is: " + JSON.stringify(event));
+    var params = {
+        topic: "fromLambda", 
+        payload: JSON.stringify(event), //event.Temperature
+        qos: 0
+    };
+
+    return iotdata.publish(params, function(err, data) {
+        if (err) {
+            console.log("If error: " + JSON.stringify(err));
+        }
+        else {
+            console.log("Success");
+        }
+    })
+};
