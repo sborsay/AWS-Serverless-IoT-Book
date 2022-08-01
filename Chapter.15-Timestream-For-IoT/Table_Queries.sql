c6310827d9c25ede892676e4e48713846491f2ed
Stephen Borsay <borsay@gmail.com>
2022-08-01 11:37:22 -0700

    Update Table_Queries.sql

--- Chapter.15-Timestream-For-IoT/Table_Queries.sql
+++ Chapter.15-Timestream-For-IoT/Table_Queries.sql
@@ -1,5 +1,4 @@
 SELECT * FROM "myIotDB44"."myIoTDB44_table" WHERE time between ago(999m) and now() ORDER BY time DESC LIMIT 10 
 
-Or a simplified query for small data tables:
 
 SELECT * FROM "myIotDB44"."myIoTDB44_table"
