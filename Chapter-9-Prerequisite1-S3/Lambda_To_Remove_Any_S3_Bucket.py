c4b2f734221c4e8fa36f05ba22de271852bd4708
Stephen Borsay <borsay@gmail.com>
2022-08-02 16:06:49 -0700

    Update Lambda_To_Remove_Any_S3_Bucket.py

--- Chapter-9-Prerequisite1-S3/Lambda_To_Remove_Any_S3_Bucket.py
+++ Chapter-9-Prerequisite1-S3/Lambda_To_Remove_Any_S3_Bucket.py
@@ -2,10 +2,10 @@ My_Bucket = '<Your-Bucket-Here>'  #Example myBucket6385
 
 import boto3
 
-
-
 def lambda_handler(event, context):
    s3 = boto3.resource('s3')
    bucket = s3.Bucket(My_Bucket)
    bucket.object_versions.delete()
    bucket.delete()
+
+   #Don't forget to give me permissions to delete S3 (create inline policy)
