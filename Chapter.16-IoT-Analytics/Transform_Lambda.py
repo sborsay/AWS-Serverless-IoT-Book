9df233916feab907fa4501ae37ffbf5c98a20f57
Stephen Borsay <borsay@gmail.com>
2022-08-04 07:47:30 -0700

    Update Transform_Lambda.py

--- Chapter.16-IoT-Analytics/Transform_Lambda.py
+++ Chapter.16-IoT-Analytics/Transform_Lambda.py
@@ -2,9 +2,8 @@
 import json
 import time
 
-
 def lambda_handler(event, context):
-    event[0]['ServersideTimestamp'] = round(time.time() * 1000)
+    event[0]['serversidetimestamp'] = round(time.time() * 1000)
 
     print event
     return event
