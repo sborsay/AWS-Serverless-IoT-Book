6bc730547e88abc216c07a724eec75b5cfeff62e
Stephen Borsay <borsay@gmail.com>
2022-08-04 07:46:57 -0700

    Update Transform_Lambda.js

--- Chapter.16-IoT-Analytics/Transform_Lambda.js
+++ Chapter.16-IoT-Analytics/Transform_Lambda.js
@@ -3,8 +3,8 @@ exports.handler = function handler(event) {
     console.log("event whole: ", event);
     console.log("event element: ", event[0]);
     
-//add timestamp to data and name it "ServersideTimestamp"
-        event[0].ServersideTimestamp = Date.now();
+//add timestamp to data and name it "serversidetimestamp"
+        event[0].serversidetimestamp = Date.now();
         console.log("event whole mod: ", event);
         
        return event;
