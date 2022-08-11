84c80a2a90b982c6d52a09f38984400f17e80868
Stephen Borsay <borsay@gmail.com>
2022-08-11 10:53:49 -0700

    Delete Transform_Lambda.py

--- Chapter.16-IoT-Analytics/Transform_Lambda.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import json
-import time
-
-def lambda_handler(event, context):
-    event[0]['serversidetimestamp'] = round(time.time() * 1000)
-
-    print event
-    return event
