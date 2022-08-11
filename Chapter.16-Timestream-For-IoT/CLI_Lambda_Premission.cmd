49ef4cd357e950cb4840edc3a7b562cd06b2cfdf
Stephen Borsay <borsay@gmail.com>
2022-08-11 10:42:35 -0700

    Create CLI_Lambda_Premission.cmd

---
+++ Chapter.16-Timestream-For-IoT/CLI_Lambda_Premission.cmd
@@ -0,0 +1,1 @@
+aws lambda add-permission --function-name <Your-Lambda-Name> --region <Your-AWS-Region> --statement-id <Some-ID> --principal iotanalytics.amazonaws.com --action lambda:InvokeFunction
