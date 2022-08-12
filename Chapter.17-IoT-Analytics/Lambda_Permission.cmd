818984bd9eef90ab2e03d9631ab0ddbc999374ab
Stephen Borsay <borsay@gmail.com>
2022-08-11 17:15:53 -0700

    Update Lambda_Permission.cmd

--- Chapter.17-IoT-Analytics/Lambda_Permission.cmd
+++ Chapter.17-IoT-Analytics/Lambda_Permission.cmd
@@ -1,1 +1,4 @@
 aws lambda add-permission --function-name <Your-Lambda-Name> --region <Your-AWS-Region> --statement-id <Some-ID> --principal iotanalytics.amazonaws.com --action lambda:InvokeFunction 
+
+
+### Look on page 46:  https://docs.aws.amazon.com/iotanalytics/latest/userguide/iotanalytics-ug.pdf
