87b33b6852e728508bc3c83d0f348ba484b02d34
Stephen Borsay <borsay@gmail.com>
2022-08-11 17:17:11 -0700

    Delete Lambda_Permission.cmd

--- Chapter.17-IoT-Analytics/Lambda_Permission.cmd
+++
@@ -1,4 +0,0 @@
-aws lambda add-permission --function-name <Your-Lambda-Name> --region <Your-AWS-Region> --statement-id <Some-ID> --principal iotanalytics.amazonaws.com --action lambda:InvokeFunction 
-
-
- Look on page 46 for complete AWS CLI command:  https://docs.aws.amazon.com/iotanalytics/latest/userguide/iotanalytics-ug.pdf
